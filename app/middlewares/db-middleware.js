0d93f795bec0a045941c835a6bd683844ab14d56
ninjascant <cvlt.mao@gmail.com>
2018-01-12 19:02:12 +0500

    Update db-middleware.js: add fininshed projects handler

--- app/middlewares/db-middleware.js
+++ app/middlewares/db-middleware.js
@@ -1,37 +1,60 @@
 module.exports = (req, res, next) => {
   req.blobs.forEach(blob => {
     const file = blob.content
-    if(blob.content.ico !== undefined) {
+    console.log(file)
+    if(file.ico !== undefined) {
       // Search if there's active phase
-      const activePhase = blob.content.ico.phases.filter(phase => phase.phase_status.search(/Active/) !== -1)
-      if (activePhase.length === 0) return
-      // This object will be sent to the dates collection
-      const datesObj = {
-        startDate: activePhase[0].dates.start_date,
-        endDate: activePhase[0].dates.end_date,
-        project: blob.content.description.project_name 
-      }
-      // Add or update datesCollection
-      req.db.collection('datesCollection').findOne({project: blob.content.description.project_name}, (err, res) => {
-        if(err) return console.log('Err while searching project', err)
-        if (res === null) {
-          req.db.collection('datesCollection').save(datesObj, (err, res) => {
-            if(err) console.log('Err while add to datesCollection', err)
-            req.db.collection('activeProjects').save(blob.content, (err, res) => {
-              console.log(err?err:`Project added to db: ${blob.content.description.project_name}`)
+      const activePhase = file.ico.phases.filter(phase => phase.phase_status.search(/Active/) !== -1)
+      if (activePhase.length !== 0) {
+        // Add or update activeProjects
+        req.db.collection('activeProjects').findOne({project: file.blockchain.project_name}, (err, res) => {
+          if(err) return console.log('Err while searching project', err)
+          if (res === null) {
+            req.db.collection('activeProjects').save(file, (err, res) => {
+              console.log(`Project added to activeProjects: ${file.blockchain.project_name}`)
               next()
             })
+          } else {
+            req.db.collection('activeProjects').update({project: file.blockchain.project_name}, file, (err, res) => {
+              if(err) return console.log('Err while update activeProjects', err)
+              console.log(`Project updated in activeProjects: ${file.blockchain.project_name}`)
+              next()
+            })
+          }
+        })
+      } else {
+        // check if there's filled raised_funds fields
+        const inWork = (file.ico.phases.filter(phase => phase.raised_funds[0].amount === 0).length === 0)
+        if (inWork) {
+          req.db.collection('finishedInWork').findOne({project: file.blockchain.project_name}, (err, res) => {
+            if (res === null) {
+              req.db.collection('finishedInWork').save(file, (err, res) => {
+                console.log(err?err:`Project added to finishedInWork: ${file.blockchain.project_name}`)
+                next()
+              })
+            } else {
+              req.db.collection('finishedInWork').update({project: file.blockchain.project_name}, file, (err, res) => {
+                if(err) return console.log('Err while update datesCollection', err)
+                console.log(`Project updated in finishedInWork: ${file.blockchain.project_name}`)
+              })
+            }
           })
         } else {
-          req.db.collection('activeProjects').update({project: blob.content.description.project_name}, blob.content, (err, res) => {
-            if(err) console.log('Err while update datesCollection', err)
-            req.db.collection('activeProjects').update({project: blob.content.description.project_name}, blob.content, (err, res) => {
-              console.log(err?err:`Project updated in db: ${blob.content.description.project_name}`)
-              next()
-            })
+          req.db.collection('finished').findOne({project: file.blockchain.project_name}, (err, res) => {
+            if (res === null) {
+              req.db.collection('finished').save(file, (err, res) => {
+                console.log(err?err:`Project added to finished: ${file.blockchain.project_name}`)
+                next()
+              })
+            } else {
+              req.db.collection('finished').update({project: file.blockchain.project_name}, file, (err, res) => {
+                if(err) return console.log('Err while update datesCollection', err)
+                console.log(`Project updated in finished: ${file.blockchain.project_name}`)
+              })
+            }
           })
         }
-      })
+      }
     } else {
       next()
     }
