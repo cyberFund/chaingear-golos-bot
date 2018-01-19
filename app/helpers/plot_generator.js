c8e780cb06b3d84edc71a111ecccd5e5c7ebd88f
ninjascant <cvlt.mao@gmail.com>
2018-01-20 00:36:12 +0500

    Add: plot_generator.js Helper function that creates charts that describe funds distribution

---
+++ app/helpers/plot_generator.js
@@ -0,0 +1,66 @@
+const canvasModule = require('canvas')
+const D3Node = require('d3-node')
+const d3n = new D3Node({ canvasModule })
+const d3 = d3n.d3 
+const fs = require('fs')
+  
+var dataset = [
+  { name: 'IE', percent: 39.10 },
+  { name: 'Chrome', percent: 32.51 },
+  { name: 'Safari', percent: 13.68 },
+  { name: 'Firefox', percent: 8.71 },
+  { name: 'Others', percent: 6.01 }
+]
+module.exports = (dataset, filename, palleteNum) => {
+  const palletes = [
+    ['#E3B1AA', '#C3D8C1', '#89C284', '#F0B95D', '#95AB99'],
+    ['#F0B5A5', '#AEA68B', '#BDCFD9', '#A2C0A0', '#EEC37F', '#40534F', ],
+    ['#F3CB6F', '#F67172', '#96DACE',  '#E7C089', '#95AB99', '#999698', '#EEC2E5',]
+  ]
+  
+  const width = 250,
+    height = 250
+
+  const canvas = d3n.createCanvas(500, 250)
+  // DBE8D9 FDF7DB
+  const context = canvas.getContext('2d')
+  context.fillStyle = "#DBE8D9";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  const pie = d3.pie()
+    .value(d => d.percent)
+    .sort(null)
+    .padAngle(.02);
+
+  const outerRadius=width/2,
+    innerRadius=height/2.4
+
+  context.translate(width / 2, height / 2)
+
+  const arc=d3.arc()
+    .outerRadius(outerRadius)
+    .innerRadius(innerRadius)
+    .context(context)
+
+  const arcs = pie(dataset)
+  arcs.forEach((d, i) => {
+    context.beginPath()
+    arc(d)
+    context.fillStyle = palletes[palleteNum][i]
+    context.fill()
+  });
+
+  context.font = "14px Helvetica";
+
+  context.beginPath();
+  arcs.forEach(arc);
+  context.strokeStyle = "#DBE8D9";
+
+  dataset.forEach((d, i) => {
+    const y = -(10 + i * 20)
+    context.fillStyle = palletes[palleteNum][i]
+    context.fillRect(150, y, 15, 15)
+    context.fillStyle = '#000'
+    context.fillText(d.name, 180, y+12)
+  })
+  canvas.pngStream().pipe(fs.createWriteStream(filename))
+}
